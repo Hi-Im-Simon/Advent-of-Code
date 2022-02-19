c7a9a9fa0863266aa86dd075307a9fc7e00080ab
Hi-Im-Simon <75808585+Hi-Im-Simon@users.noreply.github.com>
2022-02-19 15:00:45 +0100

    (1/2) 2021 day 18

--- 2021/day-18.py
+++ 2021/day-18.py
@@ -1,105 +1,83 @@
+from math import ceil
+from shlex import join
+
+
 tf = open('2021/inputs/input-00.txt')   # you can put an example input data here
 f = open('2021/inputs/input-18.txt')    # your input data
 
 
 def prep_input(f):  # edit to adjust how the program reads your files
     datas = [list(x.strip()) for x in f.readlines()]
-    for data in datas:
-        for i, char in enumerate(data):
-            if char.isdigit():
-                data[i] = int(char)
-    return datas
+    val_datas = []
+    for i in range(len(datas)):
+        depth = 0
+        val_datas.append([])
+        for char in datas[i]:
+            if char == ',':
+                continue
+            elif char == '[':
+                depth += 1
+            elif char == ']':
+                depth -= 1
+            else:
+                val_datas[i].append([depth, int(char)])
+    return val_datas
 
 
 def action_explode(data):
-    depth = -1
-    for i, char in enumerate(data):
-        if char == ',':
-            continue
-        elif char == '[':
-            depth += 1
-        elif char == ']':
-            depth -= 1
-            
-        if depth == 4:
-            start = i
-            left, right = data[i+1], data[i+3]
-            data[i] = 0
-            del data[i+1:i+5]
-            
-            i -= 1
-            while i > 0:
-                if type(data[i]) == int:
-                    data[i] += left
-                    break
-                i -= 1
-            i = start + 1
-            while i < len(data):
-                if type(data[i]) == int:
-                    data[i] += right
-                    break
-                i += 1
-            break
-    return data
+    for i, num in enumerate(data):
+        if num[0] == 4:
+            if i > 0:
+                data[i-1][1] += data[i][1]
+            if i + 1 < len(data) - 1:
+                data[i+2][1] += data[i+1][1]
+            data[i] = [data[i][0]-1, 0]
+            del data[i+1]
+            return data
+    return 0
 
 
 def action_split(data):
-    depth = -1
-    for i, char in enumerate(data):
-        if char == ',':
-            continue
-        elif char == '[':
-            depth += 1
-        elif char == ']':
-            depth -= 1
-        else:
-            if char > 9:
-                if char % 2 == 0:
-                    data = data[:i] + ['[', data[i] // 2, ',', data[i] // 2, ']'] + data[i+1:]
-                else:
-                    data = data[:i] + ['[', data[i] // 2, ',', data[i] // 2 + 1, ']'] + data[i+1:]
-                break
-    return data
+    for i, num in enumerate(data):
+        if num[1] >= 10:
+            data.insert(i+1, [num[0]+1, ceil(num[1] / 2)])
+            data[i] = [num[0]+1, int(num[1] / 2)]  
+            return data
+    return 0
 
 
-def to_snailfish(data):
-    new, last = action_explode(data), ''
-    while last != new:
-        while last != new:
-            last = new.copy()
-            new = action_explode(new)
-        last = new.copy()
-        new = action_split(new)
-    return new
+def to_snailfish(d1, d2):
+    data = d1 + d2
+    while True:
+        out = action_explode(data)
+        if type(out) == int:
+            out = action_split(data)
+            if type(out) == int:
+                return data
 
 
 def rec(data):
-    if type(data[0]) == list:
-        data[0] = rec(data[0])
-    if type(data[1]) == list:
-        data[1] = rec(data[1])
-    return 3 * data[0] + 2 * data[1]
+    for val in range(3, -1, -1):
+        for i, el in enumerate(data):
+            if el[0] == val:
+                data[i] = [val-1, data[i][1] * 3 + data[i+1][1] * 2]
+                del data[i+1]
+    return data[0][1]
+    
                 
 
 def part1(f):
     datas = prep_input(f)
-    for _ in range(20):
-        main_data = datas[0]
-        
-        i = 1
-        while i < len(datas):
-            # print('\t', ''.join([str(x) if type(x) == int else x for x in main_data]))
-            # print('+\t', ''.join([str(x) if type(x) == int else x for x in datas[i]]))
-            main_data = ['['] + main_data + [','] + datas[i] + [']']
-            main_data = to_snailfish(main_data)
-            # print('=\t', ''.join([str(x) if type(x) == int else x for x in main_data]))
-            # print('\n')
-            i += 1
-        data = eval(''.join([str(x) if type(x) == int else x for x in main_data]))
-        
-        data = rec(data)
-    
-    return data
+
+    for _ in range(10):
+        main_data = [x.copy() for x in datas[0]]
+        for i in range(1, len(datas)):
+            if i > 1:
+                for j in range(len(main_data)):
+                    main_data[j][0] += 1
+            main_data = to_snailfish(main_data, [x.copy() for x in datas[i]])
+            
+    return rec(main_data)
 
 
 def part2(f):
@@ -109,5 +87,5 @@ def part2(f):
     return None
 
 
-print(f"part 1:\n{ part1(f) }")
+print(f"part 1:\n{ part1(tf) }")
 print(f"part 2:\n{ part2(f) }")
