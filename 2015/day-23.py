18e27d2f347034c0f965aae421dcebb8214d1943
Hi-Im-Simon <75808585+Hi-Im-Simon@users.noreply.github.com>
2022-04-15 00:41:08 +0200

    (2/2) 2015 day 23

---
+++ 2015/day-23.py
@@ -0,0 +1,45 @@
+tf = open('2015/inputs/input-00.txt').readlines()   # you can put an example input data here
+f = open('2015/inputs/input-23.txt').readlines()    # your input data
+
+
+def prep_input(f):  # edit to adjust how the program reads your files
+    data = [x.split() for x in f]
+    for line in data:
+        if len(line) == 3:
+            line[1], line[2] = line[1].strip(','), int(line[2])
+        elif line[1][0] in ['+', '-']:
+            line[1] = int(line[1])
+    return data
+
+
+def part1_and_2(f, a=0, b=0):
+    data = prep_input(f)
+    
+    regs = {'a': a, 'b': b}
+    i = 0
+
+    while i < len(data):
+        match data[i][0]:
+            case 'inc':
+                regs[data[i][1]] += 1
+            case 'hlf':
+                regs[data[i][1]] //= 2
+            case 'tpl':
+                regs[data[i][1]] *= 3
+            case 'jmp':
+                i += data[i][1]
+                continue
+            case 'jie':
+                if not regs[data[i][1]] % 2:
+                    i += data[i][2]
+                    continue
+            case 'jio':
+                if regs[data[i][1]] == 1:
+                    i += data[i][2]
+                    continue
+        i += 1
+    return regs['b']
+
+
+print(f"part 1:\n{ part1_and_2(f) }")
+print(f"part 2:\n{ part1_and_2(f, 1, 0) }")
--- 2021/day-19.py
+++ 2021/day-19.py
@@ -22,62 +22,33 @@ def prep_input(f):  # edit to adjust how the program reads your files
 
 def part1(f):
     data = prep_input(f)
-    ref_data = []
+    data_tree = []
     
     for s, scanner in enumerate(data):
-        ref_data.append([])
-        for i in range(len(scanner) - 1):
-            for j in range(i + 1, len(scanner)):
-                x, y, z = [abs(scanner[i][t] - scanner[j][t]) for t in range(3)]
-                ref_data[s].append([sqrt(y**2 + sqrt(x**2 + z**2)**2), (i, j)])
+        data_tree.append({})
+        for i in range(len(scanner)):
+            data_tree[s][i] = {}
+            for j in range(len(scanner)):
+                 if i != j:
+                    x, y, z = [abs(scanner[i][t] - scanner[j][t]) for t in range(3)]
+                    data_tree[s][i][j] = sqrt(y**2 + sqrt(x**2 + z**2)**2)
     
-    current, available = 0, [x for x in range(1, len(data))]
-    
-    while len(available):
-        for other in available:
-            count = 0
-            cur, new = ref_data[current], ref_data[other]
-            
-            for el1 in new:
-                for el2 in cur:
-                    if el1[0] == el2[0]:
-                        count += 1
-                        element_new = el1[1]
-                        element_old = el2[1]
-                        # print(data[current][element_old[0]], data[current][element_old[1]])
-                        # print(data[other][element_new[0]], data[other][element_new[1]])
-            
-            if count >= 66:
-                
-                diffr_table_old = [data[current][element_old[0]][z] - data[current][element_old[1]][z] for z in range(3)]
-                diffr_table_new = [data[other][element_new[0]][z] - data[other][element_new[1]][z] for z in range(3)]
-                print(data[current][element_old[0]], data[current][element_old[1]], diffr_table_old)
-                print(data[other][element_new[0]], data[other][element_new[1]], diffr_table_new)
-                order_table = []
-                for i in range(len(diffr_table_old)):
-                    for j in range(len(diffr_table_new)):
-                        if abs(diffr_table_old[i]) == abs(diffr_table_new[j]):
-                            order_table.append(j)                        
-                neg_table = [1 if diffr_table_old[i] == diffr_table_new[i] else -1 for i in range(3)]
-                
-                print(order_table, neg_table)
-                
-                old_el = data[current][element_old[0]]
-                new_el = [data[other][element_new[0]][i] * neg_table[i] for i in order_table]
-                
-                for i in range(3):
-                    print(-(diffr_table_old[i] - old_el[i] - new_el[i]))
-                
-                print(old_el)
-                print(new_el)
-                
-                
-                
-                current = other
-                available.remove(other)
-                print(available)
-                break
-    
+    # compare each scanner with each
+    for s0 in range(len(data_tree) - 1):
+        for s1 in range(s0 + 1, len(data_tree)):
+            # for each element in scanner 0
+            for el0 in data_tree[s0].values():
+                # for each element in scanner 1
+                for el1 in data_tree[s1].values():
+                    count = 0
+                    # for each line between 2 points in element 0
+                    for line0 in el0.values():
+                        # if the same line size exists in element 1
+                        if line0 in el1.values():
+                            count += 1
+                    if count >= 11:
+                        print(s0, s1)
+                        print(count)
     return
 
 
