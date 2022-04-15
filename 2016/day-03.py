7e70487a5817423d149da63e0bc5066813e4a91e
Hi-Im-Simon <75808585+Hi-Im-Simon@users.noreply.github.com>
2022-04-15 17:35:32 +0200

    (2/2) 2016 day 3

--- 2016/day-03.py
+++ 2016/day-03.py
@@ -3,22 +3,30 @@ f = open('2016/inputs/input-03.txt').readlines()    # your input data
 
 
 def prep_input(f):  # edit to adjust how the program reads your files
-    data = [x.strip() for x in f]
+    data = [list(map(int, x.split())) for x in f]
     return data
+
+
+def count(data):
+    data = [sorted(x) for x in data]
+    ans = 0
+    for line in data:
+        if line[2] < sum(line[:2]):
+            ans += 1
+    return ans
 
 
 def part1(f):
-    data = prep_input(f)
-    
-    
-    return data
+    return count(prep_input(f))
 
 
 def part2(f):
     data = prep_input(f)
-    
-    
-    return None
+    for i in range(0, len(data), 3):
+        data[i+1][0], data[i][1] = data[i][1], data[i+1][0]
+        data[i+2][0], data[i][2] = data[i][2], data[i+2][0]
+        data[i+2][1], data[i+1][2] = data[i+1][2], data[i+2][1]
+    return count(data)
 
 
 print(f"part 1:\n{ part1(f) }")
