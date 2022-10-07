3cc09a7d5b1916e64f14824713e551c14dfd0f2f
Hi-Im-Simon <75808585+Hi-Im-Simon@users.noreply.github.com>
2022-10-07 13:19:46 +0200

    (2/2) 2016 day 13

--- 2016/day-13.py
+++ 2016/day-13.py
@@ -1,24 +1,58 @@
-tf = open('2016/inputs/input-00.txt').readlines()   # you can put an example input data here
-f = open('2016/inputs/input-13.txt').readlines()    # your input data
+tf = [open('2016/inputs/input-00.txt').readlines(), (7, 4)]   # you can put an example input data here
+f = [open('2016/inputs/input-13.txt').readlines(), (31, 39)]    # your input data
+
+ans1 = float('inf')
+fav_num = 0
+end_point = (0, 0)
+ans2 = 0
+global_visited = set()
 
 
 def prep_input(f):  # edit to adjust how the program reads your files
-    data = [x.strip() for x in f]
-    return data
+    global fav_num, end_point
+    fav_num = int(f[0][0])
+    end_point = f[1]
 
 
+def rec(x, y, steps, visited):
+    global ans1
+    if (
+        (x, y) in visited
+        or
+        x < 0 or y < 0
+        or
+        steps >= ans1
+        or
+        bin((((x + 3) * x + (2 * x * y) + (y + 1) * y) + fav_num)).count('1') % 2
+    ):
+        return
+    steps += 1
+    if (x, y) == end_point:
+        if steps < ans1:
+            ans1 = steps
+        return
+    visited.add((x, y))
+    
+    global global_visited, ans2
+    if (x, y) not in global_visited and steps <= 50:
+        global_visited.add((x, y))
+        ans2 += 1
+        
+    
+    rec(x + 1, y, steps, visited.copy())
+    rec(x, y + 1, steps, visited.copy())
+    rec(x - 1, y, steps, visited.copy())
+    rec(x, y - 1, steps, visited.copy())
+    
+    
 def part1(f):
-    data = prep_input(f)
-    
-    
-    return data
+    prep_input(f)    
+    rec(1, 1, -1, set())
+    return ans1
 
 
 def part2(f):
-    data = prep_input(f)
-    
-    
-    return None
+    return ans2
 
 
 print(f"part 1:\n{ part1(f) }")
