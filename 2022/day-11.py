a757b32b107ab844b1680308afd0148b61f4ff3d
Hi-Im-Simon <75808585+Hi-Im-Simon@users.noreply.github.com>
2022-12-11 14:22:33 +0100

    (2/2) 2022 day 11

---
+++ 2022/day-11.py
@@ -0,0 +1,87 @@
+import numpy as np
+import math
+tf = open('2022/inputs/input-00.txt').readlines()   # you can put an example input data here
+f = open('2022/inputs/input-11.txt').readlines()    # your input data
+
+
+def prep_input(f):  # edit to adjust how the program reads your files
+    data = [x.strip().split() for x in f] + [[]] # + to append the last monkey to the list
+    monkeys = []
+    
+    for line in data:
+        if line == []:
+            monkeys.append(monkey)
+        elif line[0] == 'Monkey':
+            monkey = Monkey()
+        elif line[0] == 'Starting':
+            monkey.items = [int(x.strip(',')) for x in line[2:]]
+        elif line[0] == 'Operation:':
+            monkey._operator = line[4]
+            monkey._value = (int(line[5]) if line[5] != 'old' else line[5])
+        elif line[0] == 'Test:':
+            monkey.div = int(line[3])
+        elif line[1] == 'true:':
+            monkey.throw[1] = int(line[5])
+        elif line[1] == 'false:':
+            monkey.throw[0] = int(line[5])
+    return monkeys
+
+
+class Monkey:
+    def __init__(self):
+        self.items = None
+        self._operator = None
+        self._value = None
+        self.div = None
+        self.throw = [None, None]
+        self.inspections = 0
+        
+    def calc(self, div_func):
+        if self._value == 'old':
+            val = self.items[0]
+        else:
+            val = self._value
+            
+        if self._operator == '+':
+            self.items[0] += val
+        else:
+            # self.items[0] = np.multiply(self.items[0], val)
+            self.items[0] *= val
+        self.items[0] = div_func(self.items[0])
+        self.inspections += 1
+        return self.throw[not (self.items[0] % self.div)]
+    
+
+def part1(f):
+    monkeys = prep_input(f)
+    
+    for _ in range(20):
+        for monkey in monkeys:
+            for _ in monkey.items:
+                new_monkey = monkey.calc(lambda x: x // 3)
+                monkeys[new_monkey].items.append(monkey.items[0])
+                monkey.items = monkey.items[1:]
+            
+    inspections = sorted([monkey.inspections for monkey in monkeys])
+    return inspections[-1] * inspections[-2]
+    
+
+
+def part2(f):
+    monkeys = prep_input(f)
+    
+    common_div = max(math.lcm(*[monkey.div for monkey in monkeys]), 1)
+
+    for _ in range(10000):
+        for monkey in monkeys:
+            for _ in monkey.items:
+                new_monkey = monkey.calc(lambda x: x % common_div)
+                monkeys[new_monkey].items.append(monkey.items[0])
+                monkey.items = monkey.items[1:]
+
+    inspections = sorted([monkey.inspections for monkey in monkeys])
+    return inspections[-1] * inspections[-2]
+
+
+print(f"part 1:\n{ part1(f) }")
+print(f"part 2:\n{ part2(f) }")
