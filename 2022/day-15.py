e9e32e57564597a702dc53d2d39cb6603f930c15
Hi-Im-Simon <szymol1001@gmail.com>
2022-12-16 18:49:39 +0100

    (2/2) 2022 day 15

--- 2022/day-15.py
+++ 2022/day-15.py
@@ -1,6 +1,3 @@
-import sys
-sys.setrecursionlimit(10000000)
-
 tf = open('2022/inputs/input-00.txt').readlines()   # you can put an example input data here
 f = open('2022/inputs/input-15.txt').readlines()    # your input data
 
@@ -16,7 +13,6 @@ def prep_input(f):  # edit to adjust how the program reads your files
 def part1(f, line_num):
     data = prep_input(f)
     ans_line = set()
-    
     for l in data:
         dist = abs(l[0] - l[2]) + abs(l[1] - l[3])
         if abs(l[1] - line_num) <= dist:
@@ -31,17 +27,45 @@ def part1(f, line_num):
     return len(ans_line)
 
 
-def generate(data):
-    view = [[0] * 4000000]
-    return view
-
-
 def part2(f):
     data = prep_input(f)
+    edges = dict()
     
-    view = generate(data)
-
-    return view
+    def add(coords):
+        if 0 <= coords[0] <= 4_000_000 and 0 <= coords[1] <= 4_000_000:
+            if coords not in edges:
+                edges[coords] = 1
+            else:
+                edges[coords] += 1
+    
+    for line in data:
+        border_dist = abs(line[0] - line[2]) + abs(line[1] - line[3]) + 1
+        for i in range(border_dist):
+            dist = (border_dist - i)
+            coords = [
+                (line[0] + i, line[1] - dist),
+                (line[0] - dist, line[1] - i),
+                (line[0] - i, line[1] + dist),
+                (line[0] + dist, line[1] + i)
+            ]
+            for coord in coords:
+                add(coord)
+    choices = set()
+    for edge in edges:
+        if edges[edge] >= 4:
+            choices.add(edge)
+            
+    for choice in choices.copy():
+        for line in data:
+            # check if choice is still in set during its iteration
+            if choice in choices:
+                dist_s_to_b = abs(line[0] - line[2]) + abs(line[1] - line[3])
+                dist_s_to_point = abs(line[0] - choice[0]) + abs(line[1] - choice[1])
+                if dist_s_to_b >= dist_s_to_point:
+                    choices.remove(choice)
+            else: break
+    ans = list(choices)[0]
+    return ans[0] * 4000000 + ans[1]
 
 
 print(f"part 1:\n{ part1(f, 2000000) }")
